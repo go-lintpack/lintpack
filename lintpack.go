a8c8142d3b3cf4d048ce9e701cb1bf6349882b50
Iskander Sharipov <quasilyte@gmail.com>
2018-10-08 01:26:04 +0300

    add lintpack package main structs defs
    Signed-off-by: Iskander Sharipov <quasilyte@gmail.com>

--- lintpack.go
+++ lintpack.go
@@ -1,1 +1,34 @@
 package lintpack
+
+import (
+	"go/ast"
+)
+
+// CheckerDoc holds checker structured documentation.
+type CheckerDoc struct {
+	// Summary is a short one sentence description.
+	// Should not end with a period.
+	Summary string
+
+	// Details extends summary with additional info. Optional.
+	Details string
+
+	// Before is a code snippet of code that will violate rule.
+	Before string
+
+	// After is a code snippet of fixed code that complies to the rule.
+	After string
+
+	// Note is an optional caution message or advice.
+	Note string
+}
+
+// Warning represents issue that is found by checker.
+type Warning struct {
+	// Node is an AST node that caused warning to trigger.
+	// Can be used to obtain proper error location.
+	Node ast.Node
+
+	// Text is warning message without source location info.
+	Text string
+}
