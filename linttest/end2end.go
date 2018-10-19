13c8916e2e00ff1ad0229790a4c3020a068e95ed
Iskander Sharipov <quasilyte@gmail.com>
2018-10-19 19:29:17 +0300

    linttest: move golden file impl to separate file
    Signed-off-by: Iskander Sharipov <quasilyte@gmail.com>

---
+++ linttest/end2end.go
@@ -0,0 +1,67 @@
+package linttest
+
+import (
+	"io/ioutil"
+	"strings"
+	"testing"
+)
+
+type goldenFile struct {
+	warnings map[int][]*warning
+}
+
+type warning struct {
+	matched bool
+	text    string
+}
+
+func (w warning) String() string {
+	return w.text
+}
+
+func newGoldenFile(t *testing.T, filename string) *goldenFile {
+	testData, err := ioutil.ReadFile(filename)
+	if err != nil {
+		t.Fatalf("can't find checker tests: %v", err)
+	}
+	lines := strings.Split(string(testData), "\n")
+
+	warnings := make(map[int][]*warning)
+	var pending []*warning
+
+	for i, l := range lines {
+		if m := warningDirectiveRE.FindStringSubmatch(l); m != nil {
+			pending = append(pending, &warning{text: m[1]})
+		} else if len(pending) != 0 {
+			line := i + 1
+			if commentRE.MatchString(l) {
+				// Hack to make it possible to attach directives
+				// to a proper single-line comment position.
+				line -= len(pending)
+			}
+			warnings[line] = append([]*warning{}, pending...)
+			pending = pending[:0]
+		}
+	}
+	return &goldenFile{warnings: warnings}
+}
+
+func (f *goldenFile) find(line int, text string) *warning {
+	for _, y := range f.warnings[line] {
+		if text == y.text {
+			return y
+		}
+	}
+	return nil
+}
+
+func (f *goldenFile) checkUnmatched(t *testing.T, testFilename string) {
+	for line := range f.warnings {
+		for _, w := range f.warnings[line] {
+			if w.matched {
+				continue
+			}
+			t.Errorf("%s:%d: unmatched `%s`", testFilename, line, w)
+		}
+	}
+}
--- linttest/linttest.go
+++ linttest/linttest.go
@@ -4,7 +4,6 @@ import (
 	"go/ast"
 	"go/parser"
 	"go/types"
-	"io/ioutil"
 	"path/filepath"
 	"regexp"
 	"runtime"
@@ -96,66 +95,6 @@ func stripDirectives(f *ast.File) {
 func getFilename(prog *loader.Program, f *ast.File) string {
 	// see https://github.com/golang/go/issues/24498
 	return filepath.Base(prog.Fset.Position(f.Pos()).Filename)
-}
-
-type goldenFile struct {
-	warnings map[int][]*warning
-}
-
-type warning struct {
-	matched bool
-	text    string
-}
-
-func (w warning) String() string {
-	return w.text
-}
-
-func newGoldenFile(t *testing.T, filename string) *goldenFile {
-	testData, err := ioutil.ReadFile(filename)
-	if err != nil {
-		t.Fatalf("can't find checker tests: %v", err)
-	}
-	lines := strings.Split(string(testData), "\n")
-
-	warnings := make(map[int][]*warning)
-	var pending []*warning
-
-	for i, l := range lines {
-		if m := warningDirectiveRE.FindStringSubmatch(l); m != nil {
-			pending = append(pending, &warning{text: m[1]})
-		} else if len(pending) != 0 {
-			line := i + 1
-			if commentRE.MatchString(l) {
-				// Hack to make it possible to attach directives
-				// to a proper single-line comment position.
-				line -= len(pending)
-			}
-			warnings[line] = append([]*warning{}, pending...)
-			pending = pending[:0]
-		}
-	}
-	return &goldenFile{warnings: warnings}
-}
-
-func (f *goldenFile) find(line int, text string) *warning {
-	for _, y := range f.warnings[line] {
-		if text == y.text {
-			return y
-		}
-	}
-	return nil
-}
-
-func (f *goldenFile) checkUnmatched(t *testing.T, testFilename string) {
-	for line := range f.warnings {
-		for _, w := range f.warnings[line] {
-			if w.matched {
-				continue
-			}
-			t.Errorf("%s:%d: unmatched `%s`", testFilename, line, w)
-		}
-	}
 }
 
 func newProg(t *testing.T, pkgPath string) *loader.Program {
